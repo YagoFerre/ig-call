d09e75695266d79b2bc6be1bdd06bc2cc2097507
Yago Ferreira <yagofer12@gmail.com>
2023-02-04 17:26:11 -0300

    first commit

---
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -0,0 +1,68 @@
+import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
+import { ArrowRight, Check } from 'phosphor-react'
+import { Container, Header } from '../styles'
+import { AuthError, ConnectBox, ConnectItem } from './styles'
+import { useSession, signIn } from 'next-auth/react'
+import { useRouter } from 'next/router'
+
+export default function ConnectCalendar() {
+  const session = useSession()
+  const router = useRouter()
+
+  const hasAuthError = !!router.query.error
+  const isSignedIn = session.status === 'authenticated'
+
+  async function handleSignIn() {
+    await signIn('google')
+  }
+
+  async function handleNavigateToNextStep() {
+    await router.push(`/register/time-intervals`)
+  }
+
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Conecte sua agenda!</Heading>
+        <Text>
+          Conecte o seu calendário para verificar automaticamente as horas
+          ocupadas e os novos eventos à medida em que são agendados.
+        </Text>
+        <MultiStep size={4} currentStep={2} />
+      </Header>
+
+      <ConnectBox>
+        <ConnectItem>
+          <Text>Google Agenda</Text>
+          {isSignedIn ? (
+            <Button size="sm" disabled>
+              Conectado
+              <Check />
+            </Button>
+          ) : (
+            <Button variant="secondary" size="sm" onClick={handleSignIn}>
+              Conectar
+              <ArrowRight />
+            </Button>
+          )}
+        </ConnectItem>
+
+        {hasAuthError && (
+          <AuthError size="sm">
+            Falha ao se conectar ao Google, verifique se você habilitou as
+            permissões de acesso ao Google Calendar.
+          </AuthError>
+        )}
+
+        <Button
+          type="submit"
+          disabled={!isSignedIn}
+          onClick={handleNavigateToNextStep}
+        >
+          Próximo passo
+          <ArrowRight />
+        </Button>
+      </ConnectBox>
+    </Container>
+  )
+}
---
+++ src/pages/register/update-profile/index.page.tsx
@@ -0,0 +1,94 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import {
+  Avatar,
+  Button,
+  Heading,
+  MultiStep,
+  Text,
+  TextArea,
+} from '@ignite-ui/react'
+import { GetServerSideProps } from 'next'
+import { ArrowRight } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { Container, Header } from '../styles'
+import { FormAnnotation, ProfileBox } from './styles'
+import { getServerSession } from 'next-auth/next'
+import { buildNextAuthOptions } from '../../api/auth/[...nextauth].api'
+import { useSession } from 'next-auth/react'
+import { api } from '@/src/lib/axios'
+import { useRouter } from 'next/router'
+
+const updateProfileSchema = z.object({
+  bio: z.string(),
+})
+
+type UpdateProfileData = z.input<typeof updateProfileSchema>
+
+export default function UpdateProfile() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<UpdateProfileData>({
+    resolver: zodResolver(updateProfileSchema),
+  })
+
+  const session = useSession()
+  const router = useRouter()
+
+  async function handleUpdateProfile(data: UpdateProfileData) {
+    await api.put('/users/profile', {
+      bio: data.bio,
+    })
+
+    await router.push(`/schedule/${session.data?.user.username}`)
+  }
+
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Defina sua disponibilidade</Heading>
+        <Text>Por último, uma breve descrição e uma foto de perfil.</Text>
+        <MultiStep size={4} currentStep={4} />
+      </Header>
+
+      <ProfileBox as="form" onSubmit={handleSubmit(handleUpdateProfile)}>
+        <label>
+          <Text size="sm">Foto de perfil</Text>
+          <Avatar
+            src={session.data?.user.avatar_url}
+            referrerPolicy="no-referrer"
+          />
+        </label>
+
+        <label>
+          <Text size="sm">Sobre você</Text>
+          <TextArea {...register('bio')} />
+          <FormAnnotation size="sm">
+            Fale um pouco sobre você. Isto será exibido em sua página pessoal.
+          </FormAnnotation>
+        </label>
+
+        <Button type="submit" disabled={isSubmitting}>
+          Finalizar
+          <ArrowRight />
+        </Button>
+      </ProfileBox>
+    </Container>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const session = await getServerSession(
+    req,
+    res,
+    buildNextAuthOptions(req, res),
+  )
+
+  return {
+    props: {
+      session,
+    },
+  }
+}
